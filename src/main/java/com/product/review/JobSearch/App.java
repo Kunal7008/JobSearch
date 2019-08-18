485a0617c6f7527be769ed23c251f1a0f6c874b8
Kunal7008 <kunal.bhatta513@gmail.com>
2019-08-18 21:08:43 +0530

    New Code

---
+++ src/main/java/com/product/review/JobSearch/App.java
@@ -0,0 +1,13 @@
+package com.product.review.JobSearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/review/code/SeleniumTest.java
@@ -0,0 +1,10 @@
+package com.review.code;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+@Test
+public void origin() {
+	System.out.println("get my code");
+}
+}
