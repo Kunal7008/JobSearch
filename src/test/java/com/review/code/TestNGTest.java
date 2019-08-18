7f96eb8dfc1517004a705c437bfe7f655cceb975
Kunal7008 <kunal.bhatta513@gmail.com>
2019-08-18 21:22:14 +0530

    Make some noise

--- src/test/java/com/review/code/TestNGTest.java
+++ src/test/java/com/review/code/TestNGTest.java
@@ -1,5 +1,18 @@
 package com.review.code;
 
-public class TestNGTest {
+import org.testng.annotations.Test;
 
+public class TestNGTest {
+@Test
+public void show() {
+	System.out.println("get some write");
+}
+@Test
+public void goTo() {
+	System.out.println("get some write");
+}
+@Test
+public void Appium() {
+	System.out.println("get some write");
+}
 }
